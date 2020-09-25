ae3add99eca279a464cd9348b15c383672cffc7e
chrisb3546 <chrisb3546@yahoo.com>
2020-09-25 14:21:34 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
@@ -15,6 +16,42 @@ class App extends React.Component {
     }
   }
 
+  getPets = e => {
+    
+
+    // if (this.state.filters.type !== "all") {
+    //   url += `?type=${this.state.filters.type}`}
+      axios.get('api/pets')
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+    // fetch(url)
+    // .then(resp => resp.json())
+    // .then(pets => this.newPetState(pets))
+    // .catch(err => console.log(err))
+
+  }
+
+  newPetState = (pets) => {
+    this.setState({
+      pets: Object.assign({}, this.state.pets, {
+        pets: pets
+        
+       
+      })
+    
+    })
+    
+  }
+
+  changeType = e => {
+    let newType = e.target.value
+   this.setState({
+     filters: Object.assign({}, this.state.filters,{
+       type: newType
+     })
+   })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +61,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeType}
+              onFindPetsClick={this.getPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets}/>
             </div>
           </div>
         </div>
