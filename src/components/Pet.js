a65452aa14143aabecb461c7c2fc0ad0cc12aeae
jessegan <jesse13579@gmail.com>
2020-09-25 11:06:31 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,24 +2,27 @@ import React from 'react'
 
 class Pet extends React.Component {
   render() {
+    let data = this.props.pet
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            { data.gender === 'female' ? '♀' : '♂'}
+            { data.name }
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{ data.type }</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: { data.age }</p>
+            <p>Weight: { data.weight }</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          { data.isAdopted 
+            ? <button className="ui disabled button">Already adopted</button>
+            : <button className="ui primary button" onClick={e => this.props.onAdoptPet(data.id)}>Adopt pet</button>
+          }
         </div>
       </div>
     )
